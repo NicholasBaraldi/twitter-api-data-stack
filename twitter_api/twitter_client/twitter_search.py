e935cc24b8a9661c1d74637af039d964347a3d6f
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-09-29 22:42:54 -0300

    improve code quality using black/isort

--- twitter_api/twitter_client/twitter_search.py
+++ twitter_api/twitter_client/twitter_search.py
@@ -4,41 +4,39 @@ import logging
 
 logger = logging.getLogger("twitter_search")
 
+
 class twitter:
-    """_summary_
-    """
+    """_summary_"""
+
     def __init__(self, bearer_token):
 
         self.bearer_token = bearer_token
-        self._api_base_url = (
-            f"https://api.twitter.com/2/tweets/search/recent"
-        )
+        self._api_base_url = f"https://api.twitter.com/2/tweets/search/recent"
 
-        logger.info(
-            "Class=twitter, Method=__init__ msg=Instance Created"
-        )
+        logger.info("Class=twitter, Method=__init__ msg=Instance Created")
 
-    def twitter_search(self, query : str, ntweet : int):
+    def twitter_search(self, query: str, ntweet: int):
         params = {
-            "query":query,
-            "max_results":ntweet,
-            "tweet.fields":"created_at",
-            "expansions":"author_id",
-            "user.fields":"description"
+            "query": query,
+            "max_results": ntweet,
+            "tweet.fields": "created_at",
+            "expansions": "author_id",
+            "user.fields": "description",
         }
-        tweets=[]
+        tweets = []
         headers = {"Authorization": f"Bearer {self.bearer_token}"}
-        r = requests.get(self._api_base_url, headers = headers, params = params).json()
+        r = requests.get(self._api_base_url, headers=headers, params=params).json()
         tweets.append(r)
 
-        nreq=200
+        nreq = 200
 
         while nreq > 0:
             params["next_token"] = r["meta"]["next_token"]
-            r = requests.get(self._api_base_url, headers = headers, params = params).json()
+            r = requests.get(self._api_base_url, headers=headers, params=params).json()
             print(r)
             tweets.append(r)
-            nreq-=1
+            nreq -= 1
 
-data = json.load(open('./api_keys.json'))["Bearer Token"]
-twitter(data).twitter_search('Lula', 100)
+
+data = json.load(open("./api_keys.json"))["Bearer Token"]
+twitter(data).twitter_search("Lula", 100)
