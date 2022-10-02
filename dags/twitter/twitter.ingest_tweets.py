0a8f13188b19ef1d9408eaedc644778c2510a03d
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-10-02 00:25:35 -0300

    Read twitter dataframe and load Raw
    Co-authored-by: NicholasBaraldi <nicholas.baraldi@gmail.com>

--- dags/twitter/twitter.ingest_tweets.py
+++ dags/twitter/twitter.ingest_tweets.py
@@ -1,27 +1,35 @@
 from airflow import DAG
-from airflow.operatir.python import PythonOperator 
+from airflow.operatr.python import PythonOperator
 from datetime import datetime
 from twitter_api.twitter_client.twitter_search import Twitter
-
-def create_spark_client:
-
-
-def read_twitter_df:
+from pyspark.sql import SparkSession
 
 
-    
+def create_spark_session():
+    spark_session = SparkSession.builder.getOrCreate()
+    return spark_session
+
+
+def read_twitter_df():
+    spark = create_spark_session()
+    bucket_name = "databoys"
+    file_name = "tweets_json"
+    t = Twitter()
+    query = "#HouseOfTheDragon"
+    ntweet = 100
+    nreq = 200
+    tweets = t.make_req(query, ntweet, nreq)
+    spark_tweets_df = spark.createDataFrame(data=tweets)
+    spark_tweets_df.write.json(f"s3a://{bucket_name}/Raw/{file_name}")
+    if tweets:
+        return 1
+    return 0
+
+
 with DAG(
-    'ingest_twitter', 
-    start_date = datetime(2022,9,30), 
-    schedule_interval = '0 21 * * 0', 
-    catchup = False) as dag:
-
-create_spark_client = PythonOperator(
-    task_id = 'create_spark_client'
-
-)
-
-read_twitter_df = PythonOperator(
-    task_id = 'read_twitter_df'
-
-)
+    "ingest_twitter",
+    start_date=datetime(2022, 9, 30),
+    schedule_interval="0 21 * * 0",
+    catchup=False,
+) as dag:
+    pass
