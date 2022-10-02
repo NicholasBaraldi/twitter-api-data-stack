1fc0557905db38240ff31cfac7bed1e01af6c921
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-10-02 00:23:46 -0300

    Changed returned data to tweets + users
    Co-authored-by: NicholasBaraldi <nicholas.baraldi@gmail.com>

--- twitter_api/twitter_client/twitter_search.py
+++ twitter_api/twitter_client/twitter_search.py
@@ -1,3 +1,5 @@
+from ctypes import Union
+from typing import Tuple
 import requests
 import json
 import logging
@@ -8,27 +10,27 @@ logger = logging.getLogger("twitter_search")
 class Twitter:
     """_summary_"""
 
-    def __init__(self, bearer_token: str) -> None:
+    def __init__(self) -> None:
         """Create request session and recieve authorization params.
 
         Args:
             bearer_token (str): Autentication token.
         """
-
+        bearer_token = json.load(open("./api_keys.json"))["Bearer Token"]
         self.bearer_token = bearer_token
         self._api_base_url = f"https://api.twitter.com/2/tweets/search/recent"
 
         logger.info("Class=twitter, Method=__init__ msg=Instance Created")
 
-    def _twitter_search(self, query: str, ntweet: int, nreq: int) -> list:
-        """Call the API and store requests in a list.
+    def _twitter_search(self, query: str, ntweet: int, nreq: int) -> Tuple[list, list]:
+        """Call the API and store requests in a Tuple.
 
         Args:
             query (str): Query param.
             ntweet (int): Number of tweets per request.
 
         Returns:
-            list: List of tweets.
+            Tuple[list, list]: Tuple of tweets and users.
         """
 
         params = {
@@ -39,15 +41,17 @@ class Twitter:
             "user.fields": "description",
         }
         tweets = []
+        users = []
         headers = {"Authorization": f"Bearer {self.bearer_token}"}
         r = requests.get(self._api_base_url, headers=headers, params=params).json()
 
         while nreq > 0 and "next_token" in r["meta"]:
             params["next_token"] = r["meta"]["next_token"]
             r = requests.get(self._api_base_url, headers=headers, params=params).json()
-            tweets.append(r)
+            tweets += r["data"]
+            users += r["includes"]["users"]
             nreq -= 1
-        return tweets
+        return tweets, users
 
     def make_req(self, query: str, ntweet: int, nreq: int) -> list:
         """Call _twitter_search and raises an error if reqs are not done.
@@ -63,8 +67,8 @@ class Twitter:
         Returns:
             list: List of tweets.
         """
-        tweets_list = self._twitter_search(query, ntweet, nreq)
+        tweets_list, users_list = self._twitter_search(query, ntweet, nreq)
         if tweets_list:
             logger.info("Class=twitter, Method=make_req msg=API returned successfully")
-            return tweets_list
+            return tweets_list, users_list
         raise Exception("Class=twitter, Method=make_req msg=API return failed")
